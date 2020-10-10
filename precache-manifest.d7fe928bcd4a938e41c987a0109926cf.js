3fe31350912f66bcabe0ccf4311c04db3d91314c
rishabkumar07 <rishabkumar0703gmail.com>
2020-10-10 18:02:19 +0530

    Updates

---
+++ precache-manifest.d7fe928bcd4a938e41c987a0109926cf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c59a4447d1e93e850496bdbd629de9",
+    "url": "/ipodclone/index.html"
+  },
+  {
+    "revision": "801ae9f3a07d4ce1a7e8",
+    "url": "/ipodclone/static/css/main.cb3fd9d6.chunk.css"
+  },
+  {
+    "revision": "5e3cfcb9002638e00651",
+    "url": "/ipodclone/static/js/2.e17b35d0.chunk.js"
+  },
+  {
+    "revision": "b5058408cd906224a21c4018bcdabd63",
+    "url": "/ipodclone/static/js/2.e17b35d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801ae9f3a07d4ce1a7e8",
+    "url": "/ipodclone/static/js/main.1a16e6db.chunk.js"
+  },
+  {
+    "revision": "9acd9a0b41f0dd6c08fa",
+    "url": "/ipodclone/static/js/runtime-main.255240af.js"
+  },
+  {
+    "revision": "2f3dd8df4330388957c86e7e36496fe8",
+    "url": "/ipodclone/static/media/camera2.2f3dd8df.jpg"
+  },
+  {
+    "revision": "3ba0e1fc45f2dc4a6bf75723a4846831",
+    "url": "/ipodclone/static/media/coldplay.3ba0e1fc.jpeg"
+  },
+  {
+    "revision": "ef638192ea308342976d5eb052f1e322",
+    "url": "/ipodclone/static/media/games1.ef638192.jpg"
+  },
+  {
+    "revision": "71668b41ee9469601a52925a85c34165",
+    "url": "/ipodclone/static/media/games3.71668b41.jpg"
+  },
+  {
+    "revision": "a755d8f3b7119b635012db0e7236bf42",
+    "url": "/ipodclone/static/media/settings3.a755d8f3.jpg"
+  },
+  {
+    "revision": "adbef2d178851a4fd036156d96408ff7",
+    "url": "/ipodclone/static/media/song1.adbef2d1.png"
+  },
+  {
+    "revision": "46114d9583887305fc74c3becdac0366",
+    "url": "/ipodclone/static/media/songshd.46114d95.jpg"
+  },
+  {
+    "revision": "f0ad6859150ec31a5bb424b4eee92437",
+    "url": "/ipodclone/static/media/wallpaper1.f0ad6859.jpg"
+  }
+]);
